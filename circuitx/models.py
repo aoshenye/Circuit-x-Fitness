bc48b48f311b17229b530c71b1669a69893e505b
aoshenye <aoshenye@gmail.com>
2021-03-15 01:56:11 +0000

    fix json name error remove model type, migrations

--- circuitx/models.py
+++ circuitx/models.py
@@ -1,13 +1,8 @@
 from django.db import models
 
 # Create your models here.
-
-class TrainerSpeciality(Choices):
-    running = Choice(value="Choice", name="Choice")
-
 class Trainer(models.Model):
     name = models.CharField(max_length=254)
-    types = models.CharField(max_length=255, choices=TrainerSpeciality.choices)
     description = models.TextField()
     price_per_hour = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
--- django_circuitx/settings.py
+++ django_circuitx/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'crispy_forms',
     'home',
-    'products'
+    'products',
+    'circuitx'
 ]
 
 MIDDLEWARE = [
