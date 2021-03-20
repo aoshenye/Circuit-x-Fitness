a9ce5a04284bcf9bd232a700eef61445e84f2fad
aoshenye <aoshenye@gmail.com>
2021-03-20 01:19:52 +0000

    error trainers

--- trainers/urls.py
+++ trainers/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.all_trainers, name='trainers'),
-    #path('<product_id>', views.product_detail, name='product_detail')
+    path('<trainer_id>', views.trainer_detail, name='trainer_detail')
 ]
--- trainers/views.py
+++ trainers/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Trainer
 
 # Create your views here.
+
 def all_trainers(request):
     """ show all trainers """
 
@@ -13,3 +14,14 @@ def all_trainers(request):
 
     return render(request, 'trainers/trainers.html', context)
 
+
+def trainer_detail(request, trainer_id):
+    """ A view to show individual product details """
+
+    trainer = get_object_or_404(Trainer, pk=trainer_id)
+
+    context = {
+        'trainer': trainer,
+    }
+
+    return render(request, 'trainers/trainer_detail.html', context)
