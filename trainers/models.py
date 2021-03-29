e3f7b6a590a2af03f7774052d3cf8b00c0490c4f
aoshenye <aoshenye@gmail.com>
2021-03-29 11:56:35 +0000

    final cosmetic changes/alterations

--- trainers/models.py
+++ trainers/models.py
@@ -8,6 +8,7 @@ class Trainer(models.Model):
     price_per_hour = models.DecimalField(max_digits=6, decimal_places=2)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+    
 
     def __str__(self):
         return self.name
