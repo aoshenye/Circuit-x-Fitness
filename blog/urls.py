991889d0ed0e6ba5cf40ec10223d6c7dc04e29dc
aoshenye <aoshenye@gmail.com>
2021-03-30 00:57:35 +0000

    fixed erro on bag and check out and remove

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('post_detail/<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('profile', views.profile, name='profile'),
     path('profile/comment/edit/<int:id>',views.comment_edit, name='comment_edit'),
     path('profile/comment/delete/<int:id>',views.comment_destroy, name='comment_destroy'),
@@ -12,5 +12,5 @@ urlpatterns = [
     path('sign_up', views.sign_up, name='sign_up'),
     path('logout', auth_views.LogoutView.as_view(), name='logout'),
     path('profile_index', views.profile_index, name='profile_index'),
-    path('<slug:slug>/', views.post_detail, name='post_detail')
+    path('post_detail/<slug:slug>/', views.post_detail, name='post_detail')
 ]
