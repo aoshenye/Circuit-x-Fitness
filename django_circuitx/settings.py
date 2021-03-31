808ead7d3392539056fa02a09a9d4d9c5f5797bd
aoshenye <aoshenye@gmail.com>
2021-03-31 10:59:16 +0000

    reomved secret key and set debug

--- django_circuitx/settings.py
+++ django_circuitx/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'e(25kr3yt=a3o41k%(2+*e*v!1(=*g0c3&t8v!+04ev&v3l@8d'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG ='DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['circuitx.herokuapp.com', 'localhost']
 
